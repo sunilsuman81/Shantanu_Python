312139384d66fe877d7834e728b5b7678690d628
Sunil Suman <99410256+sunilsuman81@users.noreply.github.com>
2025-07-19 13:12:30 +0530

    Add files via upload

---
+++ Shantanu_First_Python/main.py
@@ -0,0 +1,17 @@
+print("Hello world!")
+print("Hello again!")
+print("This is a test.")
+print("This is another test.")
+print("This is a third test.")
+print('my name is Shantanu Suman.')
+
+print("Shantanu\nSuman")
+print("Shantanu\nis pro in maths")
+
+print("Shantanu" + " Suman")
+print("Pro " + "boy " + "is " +"good")
+
+print ('give me some money!!!!!!!!!!!')
+print("New lines can be created with a \nand the letter")
+print(9+9+9+9+9+9+9+9+9+9+9)
+print("9" + " 9")

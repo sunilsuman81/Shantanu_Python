881e1cd4204cf76552816a194dc3f4fe8ad90fbb
Sunil Suman <99410256+sunilsuman81@users.noreply.github.com>
2025-07-27 10:49:11 +0530

    Update main.py for day 1 complete

--- Shantanu_First_Python/main.py
+++ Shantanu_First_Python/main.py
@@ -1,17 +1,43 @@
-print("Hello world!")
-print("Hello again!")
-print("This is a test.")
-print("This is another test.")
-print("This is a third test.")
-print('my name is Shantanu Suman.')
+#print("Hello world!")
+#print("Hello again!")
+#print("This is a test.")
+#print("This is another test.")
+#print("This is a third test.")
+#print('my name is Shantanu Suman.')
 
-print("Shantanu\nSuman")
-print("Shantanu\nis pro in maths")
+#print("Shantanu\nSuman")
+#print("Shantanu\nis pro in maths")
 
-print("Shantanu" + " Suman")
-print("Pro " + "boy " + "is " +"good")
+#print("Shantanu" + " Suman")
+#print("Pro " + "boy " + "is " +"good")
+#
+#print ('give me some money!!!!!!!!!!!')
+#print("New lines can be created with a \nand the letter")
+#print(9+9+9+9+9+9+9+9+9+9+9+8+9+9+9+9+9+9+9+9+9*8*8*8+9+9+9999990909090909090909039303903093093039333039390+9+9+9+9+9+9+9+9*8*8*8*8*8*8*8*8*6*6*5*7*8*9*9*8*2*10*1**8*8*8*8*8*8*8*8*8*8*0+9+9+9+9+9+9+9+9+9+9-100+9+9+9+9+9+9+9+9+9+9+9*8*8*8*8*8*8*8+000000000*8*6*6*5*7*8*9*9*8*2*10*1**8*8*8*8*8*8*8*8*8*8+8*8*8*8*8*8*8*8*8*8*0)
+#print("9" + " 9")
 
-print ('give me some money!!!!!!!!!!!')
-print("New lines can be created with a \nand the letter")
-print(9+9+9+9+9+9+9+9+9+9+9)
-print("9" + " 9")
+
+#print("Hello " + input("  What is your name? ") + " \nNice to meet you!")
+#input("Press enter to exit the program")
+#input("Press s to exit the program and q to quit the program")
+
+#name = input("What is your name? ")
+#print(name + " is a good name!")
+
+#age = input("What is your age? ")
+#print("You are " + age + " years old!")
+
+#hobby = input("What is your hobby? ")
+#print("Your hobby is " + hobby + "!")
+
+#print(len(input("what is your name?" )))
+#nama = input("What is your name? ")
+#length = len(nama)
+#print(length)
+#print("length of your name is", length)
+#print("Your name is " + nama + " and it has " + str(length) + " characters.")
+
+print("what is your favourite IPL team?")
+team = input("Enter your favourite IPL team\n ")
+city = input("which city do you live in?\n ")
+print("Your favourite IPL team is " + team + " and you live in " + city + ".")
